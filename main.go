65959d2756776116ef3c49075e312bfdd293649f
Dmitry Chestnykh <dmitry@codingrobots.com>
2012-11-06 16:12:08 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/base32"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	b, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Println(base32.StdEncoding.EncodeToString(b))
+
+}
