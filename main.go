356932bf6aefc79708b264bf73500fd9c1aabdaa
Dmitry Chestnykh <dmitry@codingrobots.com>
2015-03-01 10:36:58 +0100

    Add -l (lowercase) and -p (trim padding) flags.

--- main.go
+++ main.go
@@ -4,18 +4,35 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base32"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"strings"
+)
+
+var (
+	fLowerCase   = flag.Bool("l", false, "lower case")
+	fTrimPadding = flag.Bool("p", false, "remove padding")
 )
 
 func main() {
-	enc := base32.NewEncoder(base32.StdEncoding, os.Stdout)
+	flag.Parse()
+	var b bytes.Buffer
+	enc := base32.NewEncoder(base32.StdEncoding, &b)
 	if _, err := io.Copy(enc, os.Stdin); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 	enc.Close()
-	fmt.Println()
+	s := b.String()
+	if *fLowerCase {
+		s = strings.ToLower(s)
+	}
+	if *fTrimPadding {
+		s = strings.TrimRight(s, "=")
+	}
+	fmt.Println(s)
 }
